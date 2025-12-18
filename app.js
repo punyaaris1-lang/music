6a20addbd66fd0f227be74096cc54b2b931b9602
M Rizky Firdaus <punyaaris1@gmail.com>
2025-12-18 17:36:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const audio = document.getElementById("audio");
 const playlistEl = document.getElementById("playlist");
+const songTitle = document.getElementById("songTitle");
 const canvas = document.getElementById("visualizer");
 const ctx = canvas.getContext("2d");
 
@@ -7,6 +8,7 @@ let songs = [];
 let currentIndex = 0;
 let audioCtx, analyser, source, dataArray;
 
+// Ambil Playlist
 fetch("playlist.json")
     .then(res => res.json())
     .then(data => {
@@ -27,10 +29,16 @@ function renderPlaylist() {
 
 function playSong(index) {
     currentIndex = index;
-    audio.crossOrigin = "anonymous"; 
+    const name = songs[index].url.split('/').pop().replaceAll('%20', ' ').replace('.mp3', '');
+    
+    // Update Teks Berjalan
+    songTitle.textContent = "Playing: " + name;
+
+    audio.crossOrigin = "anonymous";
     audio.src = songs[index].url;
     audio.play();
     
+    // Update Tampilan Aktif & Scroll Otomatis
     document.querySelectorAll('li').forEach((li, i) => {
         if (i === index) {
             li.classList.add("active");
@@ -62,13 +70,12 @@ function startVisualizer() {
         requestAnimationFrame(draw);
         analyser.getByteFrequencyData(dataArray);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        let barWidth = (canvas.width / bufferLength) * 2;
+        let barWidth = (canvas.width / bufferLength) * 2.5;
         let x = 0;
         for (let i = 0; i < bufferLength; i++) {
-            // Tinggi bar dikecilkan agar pas di kotak 40px
-            let barHeight = dataArray[i] / 6; 
+            let barHeight = dataArray[i] / 5;
             ctx.fillStyle = "#ccff00";
-            ctx.fillRect(x, canvas.height - barHeight, barWidth - 1, barHeight);
+            ctx.fillRect(x, canvas.height - barHeight, barWidth - 2, barHeight);
             x += barWidth;
         }
     }
